792a03faffbd666bc7217098ef79e9e4e7ed9dd6
Baptiste <baptiste.loiseau@epita.fr>
2026-02-14 19:58:45 +0100

    Amélioration du système multijoueurÃ

--- new/gametest.py
+++ new/gametest.py
@@ -7,14 +7,7 @@ height = 500
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 
-clientNumber = 0
 
-def read_pos(str):
-    str = str.split(',')
-    return int(str[0]), int(str[1])
-
-def make_pos(tup):
-    return str(tup[0]) + ',' + str(tup[1])
 
 def redrawWindow(win, player, player2):
     win.fill((255,255,255))
@@ -26,18 +19,14 @@ def redrawWindow(win, player, player2):
 def main():
     run = True
     n = Network()
-    startPos = read_pos(n.getPos())
-    p = Player(startPos[0],startPos[1],100,100,(0,255,0))
-    p2 = Player(0, 0, 100, 100, (0, 255, 0))
+
+    p =  n.getP()
+
     clock = pygame.time.Clock()
 
     while run:
         clock.tick(60)
-
-        p2pos = read_pos(n.send(make_pos((p.x, p.y))))
-        p2.x = p2pos[0]
-        p2.y = p2pos[1]
-        p2.update()
+        p2 = n.send(p)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
